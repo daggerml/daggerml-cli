2a224322463702812761d1a62ce2a8304d2e5f11
Micha Niskin <micha.niskin@gmail.com>
2025-02-01 21:14:51 -0500

    docs and things

--- src/daggerml_cli/cli/__init__.py
+++ src/daggerml_cli/cli/__init__.py
@@ -137,15 +137,259 @@ def cli(ctx, config_dir, project_dir, repo, branch, user, query, debug):
 
 
 ###############################################################################
-# STATUS ######################################################################
+# API #########################################################################
 ###############################################################################
 
 
-@cli.command(name="status")
+@cli.group(name="api", no_args_is_help=True)
 @clickex
-def cli_status(ctx):
-    """Show the current repo, branch, etc."""
-    click.echo(jsdumps(api.status(ctx.obj), ctx.obj))
+def api_group(_):
+    """DAG builder API commands.
+    These commands are normally called only by language client libraries (eg.
+    the 'daggerml' python library)."""
+
+
+@click.argument("message")
+@click.argument("name")
+@click.option("--dump", help="Import DAG from a dump.", type=str)
+@api_group.command(name="create")
+@clickex
+def api_create(ctx, name, message, dump=None):
+    """Create a new DAG.
+    A new DAG named NAME is created with a descriptive commit MESSAGE. A token
+    is printed to stdout which can be used to invoke DAG builder API methods
+    on this DAG. If the --dump option is provided a function DAG is created."""
+    try:
+        idx = api.begin_dag(ctx.obj, name=name, message=message, dump=dump)
+        click.echo(to_json(idx))
+    except Exception as e:
+        click.echo(to_json(Error(e)))
+
+
+@click.argument("json")
+@click.argument("token")
+@api_group.command(name="invoke")
+@clickex
+def api_invoke(ctx, token, json):
+    """Invoke DAG builder API methods.
+    API methods are invoked with the TOKEN returned by the 'dag create' command
+    and JSON consisting of a serialized payload of the form:
+
+        [method, [args...] {kwargs...}]"""
+    try:
+        click.echo(to_json(api.invoke_api(ctx.obj, from_json(token), from_json(json))))
+    except Exception as e:
+        click.echo(to_json(Error(e)))
+
+
+###############################################################################
+# BRANCH ######################################################################
+###############################################################################
+
+
+@cli.group(name="branch", no_args_is_help=True)
+@clickex
+def branch_group(ctx):
+    """Branch management commands."""
+
+
+@click.argument(
+    "commit",
+    required=False,
+    shell_complete=complete(api.with_query(api.list_commit, "[*].id")),
+)
+@click.argument("name")
+@branch_group.command(name="create")
+@clickex
+def branch_create(ctx, name, commit):
+    """Create a new branch."""
+    api.create_branch(ctx.obj, name, commit)
+    click.echo(f"Created branch: {name}")
+
+
+@click.argument("name", shell_complete=complete(api.list_other_branch))
+@branch_group.command(name="delete")
+@clickex
+def branch_delete(ctx, name):
+    """Delete a branch."""
+    api.delete_branch(ctx.obj, name)
+    click.echo(f"Deleted branch: {name}")
+
+
+@branch_group.command(name="list")
+@clickex
+def branch_list(ctx):
+    """List branches."""
+    click.echo(jsdumps(api.list_branch(ctx.obj), ctx.obj))
+
+
+@click.argument("branch", shell_complete=complete(api.list_other_branch))
+@branch_group.command(name="merge")
+@clickex
+def branch_merge(ctx, branch):
+    """Merge another branch with the current one."""
+    click.echo(api.merge_branch(ctx.obj, branch))
+
+
+@click.argument("branch", shell_complete=complete(api.list_other_branch))
+@branch_group.command(name="rebase")
+@clickex
+def branch_rebase(ctx, branch):
+    """Rebase the current branch onto another one."""
+    click.echo(api.rebase_branch(ctx.obj, branch))
+
+
+###############################################################################
+# COMMIT ######################################################################
+###############################################################################
+
+
+@cli.group(name="commit", no_args_is_help=True)
+@clickex
+def commit_group(_):
+    """Commit management commands."""
+
+
+@commit_group.command(name="list")
+@clickex
+def commit_list(ctx):
+    """List commits."""
+    click.echo(jsdumps(api.list_commit(ctx.obj), ctx.obj))
+
+
+@click.option("--graph", is_flag=True, help="Print a graph of all commits.")
+@commit_group.command(name="log")
+@clickex
+def commit_log(ctx, graph):
+    """Query the commit log."""
+    return api.commit_log_graph(ctx.obj)
+
+
+@click.argument("commit", shell_complete=complete(api.with_query(api.list_commit, "[*].id")))
+@commit_group.command(name="revert")
+@clickex
+def commit_revert(ctx, commit):
+    """Revert a commit."""
+    return api.revert_commit(ctx.obj, commit)
+
+
+###############################################################################
+# CONFIG ######################################################################
+###############################################################################
+
+
+@cli.group(name="config", no_args_is_help=True)
+@clickex
+def config_group(_):
+    """Configuration settings."""
+
+
+@click.argument("repo", shell_complete=complete(api.with_query(api.list_other_repo, "[*].name")))
+@config_group.command(name="repo")
+@clickex
+def config_repo(ctx, repo):
+    """Select the repository to use."""
+    api.config_repo(ctx.obj, repo)
+    click.echo(f"Selected repository: {repo}")
+
+
+@click.argument("name", shell_complete=complete(api.list_other_branch))
+@config_group.command(name="branch")
+@clickex
+def config_branch(ctx, name):
+    """Select the branch to use."""
+    api.config_branch(ctx.obj, name)
+    click.echo(f"Selected branch: {name}")
+
+
+@click.argument("user", shell_complete=complete(api.list_other_branch))
+@config_group.command(name="user")
+@clickex
+def config_user(ctx, user):
+    """Set user name/email/etc."""
+    api.config_user(ctx.obj, user)
+    click.echo(f"Set user: {user}")
+
+
+###############################################################################
+# DAG #########################################################################
+###############################################################################
+
+
+@cli.group(name="dag", no_args_is_help=True)
+@clickex
+def dag_group(_):
+    """DAG management commands."""
+
+
+@dag_group.command(name="list")
+@clickex
+def dag_list(ctx):
+    """List DAGs."""
+    click.echo(jsdumps(api.list_dags(ctx.obj), ctx.obj))
+
+
+@click.argument("message", type=str)
+@click.argument("name", type=str, shell_complete=complete(api.with_query(api.list_dags, "[*].name")))
+@dag_group.command(name="delete")
+@clickex
+def dag_delete(ctx, name, message):
+    """Delete a DAG."""
+    ref = ([x.id for x in api.list_dags(ctx.obj) if x.name == name] or [None])[0]
+    assert ref, f"no such dag: {name}"
+    api.delete_dag(ctx.obj, name, message)
+    click.echo(f"Deleted dag: {name}")
+
+
+@click.argument("name", type=str, shell_complete=complete(api.with_query(api.list_dags, "[*].name")))
+@click.option("--output", help="Output format.", type=click.Choice(["html", "json"]))
+@dag_group.command(name="graph")
+@clickex
+def dag_graph(ctx, name, output):
+    """Visualize the DAG graph.
+    With no options the graph is written to a HTML file and a browser is launched
+    to view it. If the --output option is present a text representation of the
+    graph is written to stdout (as JSON or HTML, depending on the value specified)
+    and no browser is launched."""
+    ref = ([x.id for x in api.list_dags(ctx.obj) if x.name == name] or [None])[0]
+    assert ref, f"no such dag: {name}"
+    graph = api.describe_dag(ctx.obj, ref)
+    if output == "json":
+        click.echo(jsdumps(graph))
+    elif output == "html":
+        click.echo(api.write_dag_html(ctx.obj, graph))
+    else:
+        file = os.path.join(ctx.obj.CONFIG_DIR, "cache", "daggerml_cli", "dag.html")
+        writefile(api.write_dag_html(ctx.obj, graph), file)
+        click.echo(f"Opening browser: file://{file}")
+        click.launch(f"file://{file}")
+
+
+###############################################################################
+# INDEX #######################################################################
+###############################################################################
+
+
+@cli.group(name="index", no_args_is_help=True)
+@clickex
+def index_group(_):
+    """Index management commands."""
+
+
+@index_group.command(name="list")
+@clickex
+def index_list(ctx):
+    """List indexes."""
+    click.echo(jsdumps(api.list_indexes(ctx.obj), ctx.obj))
+
+
+@click.argument("id", shell_complete=complete(api.with_query(api.list_indexes, "[*].id")))
+@index_group.command(name="delete")
+@clickex
+def index_delete(ctx, id):
+    """Delete index."""
+    if api.delete_index(ctx.obj, Ref(f"index/{id}")):
+        click.echo(f"Deleted index: {id}")
 
 
 ###############################################################################
@@ -184,6 +428,17 @@ def ref_load(ctx, json):
     """Load a previously dumped ref into the repo."""
     ref = api.load_ref(ctx.obj, json)
     click.echo(to_json(ref))
+
+
+###############################################################################
+# REMOTE ######################################################################
+###############################################################################
+
+
+@cli.group(name="remote", no_args_is_help=True)
+@clickex
+def remote_group(ctx):
+    """Repository tracking commands."""
 
 
 ###############################################################################
@@ -263,259 +518,15 @@ def repo_gc(ctx):
 
 
 ###############################################################################
-# CONFIG ######################################################################
+# STATUS ######################################################################
 ###############################################################################
 
 
-@cli.group(name="config", no_args_is_help=True)
+@cli.command(name="status")
 @clickex
-def config_group(_):
-    """Configuration settings."""
-
-
-@click.argument("repo", shell_complete=complete(api.with_query(api.list_other_repo, "[*].name")))
-@config_group.command(name="repo")
-@clickex
-def config_repo(ctx, repo):
-    """Select the repository to use."""
-    api.config_repo(ctx.obj, repo)
-    click.echo(f"Selected repository: {repo}")
-
-
-@click.argument("name", shell_complete=complete(api.list_other_branch))
-@config_group.command(name="branch")
-@clickex
-def config_branch(ctx, name):
-    """Select the branch to use."""
-    api.config_branch(ctx.obj, name)
-    click.echo(f"Selected branch: {name}")
-
-
-@click.argument("user", shell_complete=complete(api.list_other_branch))
-@config_group.command(name="user")
-@clickex
-def config_user(ctx, user):
-    """Set user name/email/etc."""
-    api.config_user(ctx.obj, user)
-    click.echo(f"Set user: {user}")
-
-
-###############################################################################
-# BRANCH ######################################################################
-###############################################################################
-
-
-@cli.group(name="branch", no_args_is_help=True)
-@clickex
-def branch_group(ctx):
-    """Branch management commands."""
-
-
-@click.argument(
-    "commit",
-    required=False,
-    shell_complete=complete(api.with_query(api.list_commit, "[*].id")),
-)
-@click.argument("name")
-@branch_group.command(name="create")
-@clickex
-def branch_create(ctx, name, commit):
-    """Create a new branch."""
-    api.create_branch(ctx.obj, name, commit)
-    click.echo(f"Created branch: {name}")
-
-
-@click.argument("name", shell_complete=complete(api.list_other_branch))
-@branch_group.command(name="delete")
-@clickex
-def branch_delete(ctx, name):
-    """Delete a branch."""
-    api.delete_branch(ctx.obj, name)
-    click.echo(f"Deleted branch: {name}")
-
-
-@branch_group.command(name="list")
-@clickex
-def branch_list(ctx):
-    """List branches."""
-    click.echo(jsdumps(api.list_branch(ctx.obj), ctx.obj))
-
-
-@click.argument("branch", shell_complete=complete(api.list_other_branch))
-@branch_group.command(name="merge")
-@clickex
-def branch_merge(ctx, branch):
-    """Merge another branch with the current one."""
-    click.echo(api.merge_branch(ctx.obj, branch))
-
-
-@click.argument("branch", shell_complete=complete(api.list_other_branch))
-@branch_group.command(name="rebase")
-@clickex
-def branch_rebase(ctx, branch):
-    """Rebase the current branch onto another one."""
-    click.echo(api.rebase_branch(ctx.obj, branch))
-
-
-###############################################################################
-# DAG #########################################################################
-###############################################################################
-
-
-@cli.group(name="dag", no_args_is_help=True)
-@clickex
-def dag_group(_):
-    """DAG management commands."""
-
-
-@dag_group.command(name="list")
-@clickex
-def dag_list(ctx):
-    """List DAGs."""
-    click.echo(jsdumps(api.list_dags(ctx.obj), ctx.obj))
-
-
-@click.argument("message", type=str)
-@click.argument("name", type=str, shell_complete=complete(api.with_query(api.list_dags, "[*].name")))
-@dag_group.command(name="delete")
-@clickex
-def dag_delete(ctx, name, message):
-    """Delete a DAG."""
-    ref = ([x.id for x in api.list_dags(ctx.obj) if x.name == name] or [None])[0]
-    assert ref, f"no such dag: {name}"
-    api.delete_dag(ctx.obj, name, message)
-    click.echo(f"Deleted dag: {name}")
-
-
-@click.argument("name", type=str, shell_complete=complete(api.with_query(api.list_dags, "[*].name")))
-@click.option("--output", help="Output format.", type=click.Choice(["html", "json"]))
-@dag_group.command(name="graph")
-@clickex
-def dag_graph(ctx, name, output):
-    """Visualize the DAG graph.
-    With no options the graph is written to a HTML file and a browser is launched
-    to view it. If the --output option is present a text representation of the
-    graph is written to stdout (as JSON or HTML, depending on the value specified)
-    and no browser is launched."""
-    ref = ([x.id for x in api.list_dags(ctx.obj) if x.name == name] or [None])[0]
-    assert ref, f"no such dag: {name}"
-    graph = api.describe_dag(ctx.obj, ref)
-    if output == "json":
-        click.echo(jsdumps(graph))
-    elif output == "html":
-        click.echo(api.write_dag_html(ctx.obj, graph))
-    else:
-        file = os.path.join(ctx.obj.CONFIG_DIR, "cache", "daggerml_cli", "dag.html")
-        writefile(api.write_dag_html(ctx.obj, graph), file)
-        click.echo(f"Opening browser: file://{file}")
-        click.launch(f"file://{file}")
-
-
-###############################################################################
-# API #########################################################################
-###############################################################################
-
-
-@cli.group(name="api", no_args_is_help=True)
-@clickex
-def api_group(_):
-    """DAG builder API commands.
-    These commands are normally called only by language client libraries (eg.
-    the 'daggerml' python library)."""
-
-
-@click.argument("message")
-@click.argument("name")
-@click.option("--dump", help="Import DAG from a dump.", type=str)
-@api_group.command(name="create")
-@clickex
-def api_create(ctx, name, message, dump=None):
-    """Create a new DAG.
-    A new DAG named NAME is created with a descriptive commit MESSAGE. A token
-    is printed to stdout which can be used to invoke DAG builder API methods
-    on this DAG. If the --dump option is provided a function DAG is created."""
-    try:
-        idx = api.begin_dag(ctx.obj, name=name, message=message, dump=dump)
-        click.echo(to_json(idx))
-    except Exception as e:
-        click.echo(to_json(Error(e)))
-
-
-@click.argument("json")
-@click.argument("token")
-@api_group.command(name="invoke")
-@clickex
-def api_invoke(ctx, token, json):
-    """Invoke DAG builder API methods.
-    API methods are invoked with the TOKEN returned by the 'dag create' command
-    and JSON consisting of a serialized payload of the form:
-
-        [method, [args...] {kwargs...}]"""
-    try:
-        click.echo(to_json(api.invoke_api(ctx.obj, from_json(token), from_json(json))))
-    except Exception as e:
-        click.echo(to_json(Error(e)))
-
-
-###############################################################################
-# INDEX #######################################################################
-###############################################################################
-
-
-@cli.group(name="index", no_args_is_help=True)
-@clickex
-def index_group(_):
-    """Index management commands."""
-
-
-@index_group.command(name="list")
-@clickex
-def index_list(ctx):
-    """List indexes."""
-    click.echo(jsdumps(api.list_indexes(ctx.obj), ctx.obj))
-
-
-@click.argument("id", shell_complete=complete(api.with_query(api.list_indexes, "[*].id")))
-@index_group.command(name="delete")
-@clickex
-def index_delete(ctx, id):
-    """Delete index."""
-    if api.delete_index(ctx.obj, Ref(f"index/{id}")):
-        click.echo(f"Deleted index: {id}")
-
-
-###############################################################################
-# COMMIT ######################################################################
-###############################################################################
-
-
-@cli.group(name="commit", no_args_is_help=True)
-@clickex
-def commit_group(_):
-    """Commit management commands."""
-
-
-@commit_group.command(name="list")
-@clickex
-def commit_list(ctx):
-    """List commits."""
-    click.echo(jsdumps(api.list_commit(ctx.obj), ctx.obj))
-
-
-@click.option("--graph", is_flag=True, help="Print a graph of all commits.")
-@commit_group.command(name="log")
-@clickex
-def commit_log(ctx, graph):
-    """Query the commit log."""
-    return api.commit_log_graph(ctx.obj)
-
-
-@click.argument("commit", shell_complete=complete(api.with_query(api.list_commit, "[*].id")))
-@commit_group.command(name="revert")
-@clickex
-def commit_revert(ctx, commit):
-    """Revert a commit."""
-    return api.revert_commit(ctx.obj, commit)
+def cli_status(ctx):
+    """Show the current repo, branch, etc."""
+    click.echo(jsdumps(api.status(ctx.obj), ctx.obj))
 
 
 ###############################################################################
